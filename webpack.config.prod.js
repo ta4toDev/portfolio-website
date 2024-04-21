fc6d61da8cc20845b2be0a4fae7df7a6bd9f6a7b
ta4toDev <irina.jaudszims@gmail.com>
2024-04-21 13:40:58 +0200

    renamed dist to public folder

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,9 +2,13 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
